9ed721c61111e00c2f793b53b082c41edc809cc5
Tobias Fischer <tobb10001@gmail.com>
2022-05-31 16:45:30 +0200

    add main plugin file

---
+++ h4a-wordpress.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WordPress Handball4All Client
+ * Description: Daten von handball4all.de anzeigen.
+ * Version: 0.0.0
+ * Requires PHP: 8.0
+ * Author: Tobias Fischer
+ * Licence: LGPL-3.0-only
+ * Licence URI: https://www.gnu.org/licenses/lgpl-3.0.html.en
+ */
