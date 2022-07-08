2e5f5e95c9d734f9a4dae89b9dae6365b2305ad8
Tobias Fischer <tobb10001@gmail.com>
2022-07-08 19:57:00 +0200

    add WpHttpClient class

---
+++ src/Util/WpHttpClient.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tobb10001\H4aWordpress\Util;
+
+use Tobb10001\H4aIntegration\Exceptions\HttpException;
+use Tobb10001\H4aIntegration\Util\HttpClientInterface;
+use WP_Error;
+
+class WpHttpClient implements HttpClientInterface
+{
+    public function get(string $url): string
+    {
+        $response = wp_remote_get($url);
+        if ($response instanceof WP_Error) {
+            throw new HttpException("Could not retrieve $url due to an error: " . $response->get_error_message());
+        }
+
+        $body = wp_remote_retrieve_body($response);
+
+        if ($body instanceof WP_Error) {
+            throw new HttpException("COuld not retrieve $url due to an error: " . $body->get_error_message());
+        }
+
+        return $body;
+    }
+
+    /**
+     * @SuppressWarnings("boolean")
+     */
+    public function getJson(
+        string $url,
+        ?bool $associative = true,
+        int $depth = 512,
+        int $flags = 0
+    ): mixed {
+        return json_decode($this->get($url), $associative, $depth, $flags);
+    }
+}
