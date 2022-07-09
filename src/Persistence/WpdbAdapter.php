77f89c8d7099cde83281e464d7678360acdd8899
Tobias Fischer <tobb10001@gmail.com>
2022-07-09 13:55:05 +0200

    refactor WpdbAdapter and rename table with additional prefix

--- src/Persistence/WpdbAdapter.php
+++ src/Persistence/WpdbAdapter.php
@@ -41,153 +41,7 @@ class WpdbAdapter implements PersistenceInterface
     }
     /** endregion */
 
-    /** region Table Creation */
-    /**
-     * Construct the "CREATE TABLE" query for the teams table.
-     */
-    private function queryCreateTableTeams(): string
-    {
-        return <<<SQL
-			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}teams (
-				id INTEGER PRIMARY KEY AUTO_INCREMENT,
-				internalName VARCHAR(255) NOT NULL,
-				identificators VARCHAR(255) NOT NULL,
-				leagueUrl VARCHAR(255) NULL,
-				cupUrl VARCHAR(255) NULL
-			);
-SQL;
-    }
-
-    /**
-     * Create the teams table.
-     */
-    private function createTableTeams(): bool
-    {
-        return (bool) $this->wpdb->query(self::queryCreateTableTeams());
-    }
-
-    /**
-     * Construct the "CREATE TABLE" query for the league data table.
-     */
-    private function queryCreateTableLeagueMetadata(): string
-    {
-        return <<<SQL
-			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}leaguemetadata (
-				teamid INTEGER NOT NULL,
-				id INTEGER PRIMARY KEY AUTO_INCREMENT,
-				name VARCHAR(255) NOT NULL,
-				sname VARCHAR(255) NOT NULL,
-				headline1 VARCHAR(255) NOT NULL,
-				headline2 VARCHAR(255) NOT NULL,
-				actualized VARCHAR(255) NOT NULL,
-				repUrl VARCHAR(255) NOT NULL,
-                scoreShownPerGame BOOLEAN NOT NULL,
-				CONSTRAINT fk_leaguemetadata_team
-					FOREIGN KEY (teamid) REFERENCES {$this->wpdb->prefix}teams(id)
-					ON DELETE CASCADE ON UPDATE CASCADE
-			);
-SQL;
-    }
-
-    /**
-     * Create the metadata table.
-     */
-    private function createTableLeagueMetadata(): bool
-    {
-        return (bool) $this->wpdb->query(self::queryCreateTableLeagueMetadata());
-    }
-
-    /**
-     * Construct the "CREATE TABLE" query for the games table.
-     */
-    private function queryCreateTableGames(): string
-    {
-        return <<<SQL
-			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}games (
-				metadataid INTEGER NOT NULL,
-				id INTEGER PRIMARY KEY AUTO_INCREMENT,
-				gID VARCHAR(255) NOT NULL,
-				sGID VARCHAR(255) NULL DEFAULT NULL,
-				gNo VARCHAR(255) NOT NULL,
-				live BOOLEAN NOT NULL,
-				gToken VARCHAR(255) NULL DEFAULT NULL,
-				gAppid VARCHAR(255) NOT NULL,
-				gDate VARCHAR(255) NOT NULL,
-				gWDay VARCHAR(255) NOT NULL,
-				gTime VARCHAR(255) NOT NULL,
-				gGymnasiumID VARCHAR(255) NOT NULL,
-				gGymnasiumNo VARCHAR(255) NOT NULL,
-				gGymnasiumName VARCHAR(255) NOT NULL,
-				gGymnasiumPostal VARCHAR(255) NOT NULL,
-				gGymnasiumTown VARCHAR(255) NOT NULL,
-				gGymnasiumStreet VARCHAR(255) NOT NULL,
-				gHomeTeam VARCHAR(255) NOT NULL,
-				gGuestTeam VARCHAR(255) NOT NULL,
-				gHomeGoals INT NULL DEFAULT NULL,
-				gGuestGoals INT NULL DEFAULT NULL,
-				gHomeGoals_1 INT NULL DEFAULT NULL,
-				gGuestGoals_1 INT NULL DEFAULT NULL,
-				gHomePoints INT NULL DEFAULT NULL,
-				gGuestPoints INT NULL DEFAULT NULL,
-				gComment VARCHAR(255) NOT NULL,
-				gGroupsortTxt VARCHAR(255) NOT NULL,
-				gReferee VARCHAR(255) NOT NULL,
-				robotextstate VARCHAR(255) NOT NULL,
-				CONSTRAINT fk_games_leaguemetadata
-					FOREIGN KEY (metadataid) REFERENCES {$this->wpdb->prefix}leaguemetadata(id)
-					ON UPDATE CASCADE ON DELETE CASCADE
-			);
-SQL;
-    }
-
-    /**
-     * Create the games table.
-     */
-    private function createTableGames(): bool
-    {
-        return (bool) $this->wpdb->query(self::queryCreateTableGames());
-    }
-
-    /**
-     * Construct the "CREATE TABLE" query for the tabscores table.
-     */
-    private function queryCreateTableTabScores(): string
-    {
-        return <<<SQL
-			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}tabscores (
-				metadataid INTEGER NOT NULL,
-                id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
-				tabScore INT NOT NULL,
-				tabTeamID VARCHAR(255) NOT NULL,
-				tabTeamname VARCHAR(255) NOT NULL,
-				liveTeam BOOLEAN NOT NULL,
-				numPlayedGames INT NOT NULL,
-				numWonGames INT NOT NULL,
-				numEqualGames INT NOT NULL,
-				numLostGames INT NOT NULL,
-				numGoalsShot INT NOT NULL,
-				numGoalsGot INT NOT NULL,
-				pointsPlus INT NOT NULL,
-				pointsMinus INT NOT NULL,
-				pointsPerGame10 VARCHAR(255) NOT NULL,
-				numGoalsDiffperGame VARCHAR(255) NOT NULL,
-				numGoalsShotperGame VARCHAR(255) NOT NULL,
-				posCriterion VARCHAR(255) NOT NULL,
-				CONSTRAINT fk_tabscore_metadata
-					FOREIGN KEY (metadataid) REFERENCES {$this->wpdb->prefix}leaguemetadata(id)
-					ON UPDATE CASCADE ON DELETE CASCADE
-			);
-SQL;
-    }
-
-    /**
-     * Create the games table.
-     */
-    private function createTableTabScores(): bool
-    {
-        return (bool) $this->wpdb->query(self::queryCreateTableTabScores());
-    }
-
+    /** region table management */
     /**
      * Create the database tables, that are needed to store the desired data.
      */
@@ -222,11 +76,133 @@ SQL;
             // if one fails, the others will still be attempted, but the result
             // stays false
             $result = $this->wpdb->query(
-                "DROP TABLE IF EXISTS {$this->wpdb->prefix}{$table}"
+                "DROP TABLE IF EXISTS {$this->wpdb->prefix}h4ac_{$table}"
             ) && $result;
         }
 
         return $result;
     }
     /** endregion */
+
+    /**
+     * Create the teams table.
+     */
+    private function createTableTeams(): bool
+    {
+        return (bool) $this->wpdb->query(
+            <<<SQL
+			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}h4ac_teams (
+				id INTEGER PRIMARY KEY AUTO_INCREMENT,
+				internalName VARCHAR(255) NOT NULL,
+				identificators VARCHAR(255) NOT NULL,
+				leagueUrl VARCHAR(255) NULL,
+				cupUrl VARCHAR(255) NULL
+			);
+SQL
+        );
+    }
+
+    /**
+     * Create the metadata table.
+     */
+    private function createTableLeagueMetadata(): bool
+    {
+        return (bool) $this->wpdb->query(
+            <<<SQL
+			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}h4ac_leaguemetadata (
+				teamid INTEGER NOT NULL,
+				id INTEGER PRIMARY KEY AUTO_INCREMENT,
+				name VARCHAR(255) NOT NULL,
+				sname VARCHAR(255) NOT NULL,
+				headline1 VARCHAR(255) NOT NULL,
+				headline2 VARCHAR(255) NOT NULL,
+				actualized VARCHAR(255) NOT NULL,
+				repUrl VARCHAR(255) NOT NULL,
+                scoreShownPerGame BOOLEAN NOT NULL,
+				CONSTRAINT fk_leaguemetadata_team
+					FOREIGN KEY (teamid) REFERENCES {$this->wpdb->prefix}h4ac_teams(id)
+					ON DELETE CASCADE ON UPDATE CASCADE
+			);
+SQL
+        );
+    }
+
+    /**
+     * Create the games table.
+     */
+    private function createTableGames(): bool
+    {
+        return (bool) $this->wpdb->query(
+            <<<SQL
+			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}h4ac_games (
+				metadataid INTEGER NOT NULL,
+				id INTEGER PRIMARY KEY AUTO_INCREMENT,
+				gID VARCHAR(255) NOT NULL,
+				sGID VARCHAR(255) NULL DEFAULT NULL,
+				gNo VARCHAR(255) NOT NULL,
+				live BOOLEAN NOT NULL,
+				gToken VARCHAR(255) NULL DEFAULT NULL,
+				gAppid VARCHAR(255) NOT NULL,
+				gDate VARCHAR(255) NOT NULL,
+				gWDay VARCHAR(255) NOT NULL,
+				gTime VARCHAR(255) NOT NULL,
+				gGymnasiumID VARCHAR(255) NOT NULL,
+				gGymnasiumNo VARCHAR(255) NOT NULL,
+				gGymnasiumName VARCHAR(255) NOT NULL,
+				gGymnasiumPostal VARCHAR(255) NOT NULL,
+				gGymnasiumTown VARCHAR(255) NOT NULL,
+				gGymnasiumStreet VARCHAR(255) NOT NULL,
+				gHomeTeam VARCHAR(255) NOT NULL,
+				gGuestTeam VARCHAR(255) NOT NULL,
+				gHomeGoals INT NULL DEFAULT NULL,
+				gGuestGoals INT NULL DEFAULT NULL,
+				gHomeGoals_1 INT NULL DEFAULT NULL,
+				gGuestGoals_1 INT NULL DEFAULT NULL,
+				gHomePoints INT NULL DEFAULT NULL,
+				gGuestPoints INT NULL DEFAULT NULL,
+				gComment VARCHAR(255) NOT NULL,
+				gGroupsortTxt VARCHAR(255) NOT NULL,
+				gReferee VARCHAR(255) NOT NULL,
+				robotextstate VARCHAR(255) NOT NULL,
+				CONSTRAINT fk_games_leaguemetadata
+					FOREIGN KEY (metadataid) REFERENCES {$this->wpdb->prefix}h4ac_leaguemetadata(id)
+					ON UPDATE CASCADE ON DELETE CASCADE
+			);
+SQL
+        );
+    }
+
+    /**
+     * Create the games table.
+     */
+    private function createTableTabScores(): bool
+    {
+        return (bool) $this->wpdb->query(
+            <<<SQL
+			CREATE TABLE IF NOT EXISTS {$this->wpdb->prefix}h4ac_tabscores (
+				metadataid INTEGER NOT NULL,
+                id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+				tabScore INT NOT NULL,
+				tabTeamID VARCHAR(255) NOT NULL,
+				tabTeamname VARCHAR(255) NOT NULL,
+				liveTeam BOOLEAN NOT NULL,
+				numPlayedGames INT NOT NULL,
+				numWonGames INT NOT NULL,
+				numEqualGames INT NOT NULL,
+				numLostGames INT NOT NULL,
+				numGoalsShot INT NOT NULL,
+				numGoalsGot INT NOT NULL,
+				pointsPlus INT NOT NULL,
+				pointsMinus INT NOT NULL,
+				pointsPerGame10 VARCHAR(255) NOT NULL,
+				numGoalsDiffperGame VARCHAR(255) NOT NULL,
+				numGoalsShotperGame VARCHAR(255) NOT NULL,
+				posCriterion VARCHAR(255) NOT NULL,
+				CONSTRAINT fk_tabscore_metadata
+					FOREIGN KEY (metadataid) REFERENCES {$this->wpdb->prefix}h4ac_leaguemetadata(id)
+					ON UPDATE CASCADE ON DELETE CASCADE
+			);
+SQL
+        );
+    }
 }
